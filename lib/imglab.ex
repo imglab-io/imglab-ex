dfe644f36d7eac978a2c40b01b509e814b89bf90
José Francisco Calvo <josefranciscocalvo@gmail.com>
2021-11-18 10:42:48 +0100

    Initial commit

---
+++ lib/imglab.ex
@@ -0,0 +1,18 @@
+defmodule Imglab do
+  @moduledoc """
+  Documentation for `Imglab`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Imglab.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/imglab_test.exs
@@ -0,0 +1,8 @@
+defmodule ImglabTest do
+  use ExUnit.Case
+  doctest Imglab
+
+  test "greets the world" do
+    assert Imglab.hello() == :world
+  end
+end
